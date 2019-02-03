f29675eb70622afdad2b0c075cca0ab3860333b0
Georg Icking-Konert <icking@onlinehome.de>
2019-02-03 09:16:11 +0100

    check uint8_t etc.
    Check for previous defnitions of uint8_t etc.

--- Header/STM8AF_STM8S.h
+++ Header/STM8AF_STM8S.h
@@ -227,28 +227,39 @@
 -----------------------------------------------------------------------------*/
 #if defined(_COSMIC_) || defined(_RAISONANCE_) || defined(_IAR_)
 
-  // signed integer types
-  typedef   signed char     int8_t;
-  typedef   signed short    int16_t;
-  typedef   signed long     int32_t;
-  #define   INT8_MIN        (-128)
-  #define   INT8_MAX        (127)
-  #define   INT16_MIN       (-32767-1)
-  #define   INT16_MAX       (32767)
-  #define   INT32_MIN       (-2147483647L-1)
-  #define   INT32_MAX       (2147483647L)
+  // skip if already defined
+  #ifndef INT8_MAX
 
-  // unsigned integer types
-  typedef   unsigned char   uint8_t;
-  typedef   unsigned short  uint16_t;
-  typedef   unsigned long   uint32_t;
-  #define   UINT8_MIN       (0)
-  #define   UINT8_MAX       (255)
-  #define   UINT16_MIN      (0)
-  #define   UINT16_MAX      (65535)
-  #define   UINT32_MIN      (0)
-  #define   UINT32_MAX      (4294967295UL)
+    // compiler specific --> If possible, use <stdint.h> from compiler
+    typedef signed long     int32_t;
+    typedef signed short    int16_t;
+    typedef signed char     int8_t; 
+  
+    typedef unsigned long   uint32_t;
+    typedef unsigned short  uint16_t;
+    typedef unsigned char   uint8_t; 
+  
+  
+    // define min/max values
+    #define 	INT8_MAX   0x7f
+    #define 	INT8_MIN   (-INT8_MAX - 1)
+  
+    #define 	UINT8_MAX   0xFF
+    #define 	UINT8_MIN   0
+  
+    #define 	INT16_MAX   0x7fff
+    #define 	INT16_MIN   (-INT16_MAX - 1)
+  
+    #define 	UINT16_MAX  0xFFFF
+    #define 	UINT16_MIN  0
+  
+    #define 	INT32_MAX   0x7fffffffL
+    #define 	INT32_MIN   (-INT32_MAX - 1L)
 
+    #define 	UINT32_MAX  0xFFFFFFFF
+    #define 	UINT32_MIN  0
+
+  #endif // INT8_MAX
 
 #elif defined(_SDCC_)
 
