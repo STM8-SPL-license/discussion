44cbfeca6b6534fd27c759f9e623a9b6110f3871
Georg Icking-Konert <icking@onlinehome.de>
2019-03-16 09:34:14 +0100

    added dummy example to check device header completely
    added dummy example to check device header completely

---
+++ Header/examples/check_header/main.c
@@ -0,0 +1,17 @@
+/**********************
+  check device header for all peripherals, using
+  (virtual) umbrella device
+**********************/
+
+/*----------------------------------------------------------
+    INCLUDE FILES
+----------------------------------------------------------*/
+#include "../../STM8A_all-peripherals.h"
+
+
+////////
+// main routine
+////////
+void main(void) {
+
+} // main()
